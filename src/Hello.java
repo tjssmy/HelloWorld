b68aef0bcb00515a59285f342cd9fcedbfc88073
Tom Smy <tjssmy@gmail.com>
2013-05-25 16:16:03 -0400

    update

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@
 public class Hello {
 	private static Integer In = new Integer(5);
 	public static void main(String[] args) {
-		System.out.println(In.toString());
+		System.out.format("here is %d\n", In);
 	      System.out.println("Hello, World");
 	   }
 }
