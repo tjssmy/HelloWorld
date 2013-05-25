0447c93a1c21102f2b235430615bfe465c26318e
Tom Smy <tjssmy@gmail.com>
2013-05-25 16:35:33 -0400

    update

--- src/Hello.java
+++ src/Hello.java
@@ -1,8 +1,9 @@
+package src;
 
 public class Hello {
 	private static Integer In = new Integer(5);
 	public static void main(String[] args) {
-		System.out.format("here is %d\n", In);
+		System.out.format("here  a new is %d\n", In);
 	      System.out.println("Hello, World");
 	   }
 }
