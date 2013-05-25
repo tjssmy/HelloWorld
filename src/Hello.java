4ada8e7ef4a1fa55a5c971ed7d8b43b9ba71023d
Tom Smy <tjssmy@gmail.com>
2013-05-25 16:11:18 -0400

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+
+public class Hello {
+	private static Integer In = new Integer(5);
+	public static void main(String[] args) {
+		System.out.println(In.toString());
+	      System.out.println("Hello, World");
+	   }
+}
